cfc85687302b21d858e36cb7b96975414b92e3a7
KIETTA-TAK <trananhkiet21082005@gmail.com>
2025-05-24 14:33:20 +0700

    create: create entity

---
+++ src/java/entity/Fine.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entity;
+
+/**
+ *
+ * @author asus
+ */
+public class Fine {
+    private int id;
+    private BorrowRecord borrowRecord;
+    private Long fineAmount;
+    private String paidStatus;
+
+      public Fine() {
+      }
+
+      public Fine(int id, BorrowRecord borrowRecord, Long fineAmount, String paidStatus) {
+            this.id = id;
+            this.borrowRecord = borrowRecord;
+            this.fineAmount = fineAmount;
+            this.paidStatus = paidStatus;
+      }
+
+      public int getId() {
+            return id;
+      }
+
+      public void setId(int id) {
+            this.id = id;
+      }
+
+      public BorrowRecord getBorrowRecord() {
+            return borrowRecord;
+      }
+
+      public void setBorrowRecord(BorrowRecord borrowRecord) {
+            this.borrowRecord = borrowRecord;
+      }
+
+      public Long getFineAmount() {
+            return fineAmount;
+      }
+
+      public void setFineAmount(Long fineAmount) {
+            this.fineAmount = fineAmount;
+      }
+
+      public String getPaidStatus() {
+            return paidStatus;
+      }
+
+      public void setPaidStatus(String paidStatus) {
+            this.paidStatus = paidStatus;
+      }
+
+
+}
